d3d1a99e209f0ced13a881e13e95332d43ce90df
DennyWassenaar <info@dennywassenaar.nl>
2014-09-11 14:54:46 +0200

    no message

---
+++ Les 1/includes/scripts/main.js
@@ -0,0 +1,12 @@
+function persoon(name, speak) {
+    this.name = naam; 
+    this.speak = praten;
+
+    function speak(speak) {
+        this.speak = name;
+    }
+}
+
+var Bob = new person("Bob");
+
+console.log(Bob);

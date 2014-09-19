2afbf42fdfb871db9f10f233709771d58072cf25
DennyWassenaar <info@dennywassenaar.nl>
2014-09-19 16:08:01 +0200

    no message

--- Opdrachten/WebApp/static/js/app.js
+++ Opdrachten/WebApp/static/js/app.js
@@ -1,8 +1,33 @@
 (function () {
-    routie('users', function () {
-        //this gets called when hash == #users
-    });
     
+    var content = {
+        about : {
+            titel: "",
+            description: ""
+        },
+        movies : ["Shawshank Redemption","lol"]
+    };
+    
+    var WebApp = WebApp || {};
 
+    WebApp.controller = {
+        //method
+        init: function () {
+            routie({
+                'about': function () {
+                    console.log("about pagina jongen");
+                },
+                'movies': function () {
+                    console.log("movies pagina jongen");
+                }
+            });
+            //console.log("draait");
+        },
+        test: function () {
+            console.log("bier");
+        }
+    };
+    //WebApp.controller.test();
 
-});
+WebApp.controller.init();
+})();

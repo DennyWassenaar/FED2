6e9d423798bb3a57985072e26f8ba2631f5b66fe
DennyWassenaar <info@dennywassenaar.nl>
2014-09-22 14:17:47 +0200

    no message

--- Opdrachten/WebApp/static/js/app.js
+++ Opdrachten/WebApp/static/js/app.js
@@ -1,33 +1,62 @@
 (function () {
-    
+
     var content = {
-        about : {
-            titel: "",
-            description: ""
+        about: {
+            titel: "About this app",
+            description: "blabla bla"
         },
-        movies : ["Shawshank Redemption","lol"]
+        movies: [{
+            "title": "Shawshank Redemption",
+            "releaseDate": "14 October 1994",
+            "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+            "cover": "shawshank-redemption.jpg"
+        }, {
+            "title": "The Godfather",
+            "releaseDate": "24 March 1972",
+            "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+            "cover": "the-godfather.jpg"
+        }, {
+            "title": "Pulp Fiction",
+            "releaseDate": "14 October 1994",
+            "description": "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+            "cover": "pulp-fiction.jpg"
+        }, {
+            "title": "The Dark Knight",
+            "releaseDate": "18 July 2008",
+            "description": "When Batman, Gordon and Harvey Dent launch an assault on the mob, they let the clown out of the box, the Joker, bent on turning Gotham on itself and bringing any heroes down to his level.",
+            "cover": "the-dark-knight.jpg"
+        }]
     };
-    
     var WebApp = WebApp || {};
+
+    WebApp.sections = {
+        init: function () {
+            console.log("geinitieerd");
+        },
+        about: function () {
+            console.log("about");
+        },
+        movies: function () {
+            console.log("movies");
+        }
+
+    }
 
     WebApp.controller = {
         //method
         init: function () {
             routie({
-                'about': function () {
-                    console.log("about pagina jongen");
+                "about": function () {
+                    WebApp.sections.about();
                 },
-                'movies': function () {
-                    console.log("movies pagina jongen");
+                "movies": function () {
+                    WebApp.sections.movies();
                 }
             });
-            //console.log("draait");
-        },
-        test: function () {
-            console.log("bier");
+            console.log("draait");
         }
     };
     //WebApp.controller.test();
 
-WebApp.controller.init();
+    WebApp.controller.init();
 })();
